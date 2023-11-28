c6f0c24d5ba80bcf2e906ba4f674a20868c66340
jc000222 <1519793475@qq.com>
2023-11-28 17:07:05 -0500

    'p3'

--- src/hero_data/cli.py
+++ src/hero_data/cli.py
@@ -1,5 +1,8 @@
 """Run Hero_data CLI"""
 from hero_data.Data_Sum.data_sum import Reader
+from hero_data.EDA.per_colum import EDA_per_column
+from hero_data.EDA.data_cleaning import Clean
+from hero_data.Inference.Infer import Analysis
 
 
 def main():
@@ -12,14 +15,14 @@ def main():
 
     while True:
         print("-----------------------------------")
-        print(
-            "(1)Part 1: Data summary\n(2)Part 2: Using an API\n(3)Part 3: Web Scraping\n(4)Part 4: Analyze dataset"
-        )
-        print("(5)Extra: Selenium\n(0)Exit")
+        print("(1)Part 1: Data summary\n(2)Part 2: EDA\n(3)Inference")
+        print("(0)Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
             Datasummary()
+        elif choice == "2":
+            EDA()
         elif choice == "0":
             print("Exiting...")
             break
@@ -37,8 +40,60 @@ def Datasummary():
     df_comic_raw = reader_comic.combiner()
     print(df_comic_raw.head())
 
-    data = "hero_data/data/marvelvsdc.csv"
+    data = input("Enter the csv: ")
 
     reader_movie = Reader(data)
     df_movie_raw = reader_movie.reader()
     print(df_movie_raw.head())
+
+
+def EDA():
+    data_url = [
+        "https://raw.githubusercontent.com/fivethirtyeight/data/master/comic-characters/dc-wikia-data.csv",
+        "https://raw.githubusercontent.com/fivethirtyeight/data/master/comic-characters/marvel-wikia-data.csv",
+    ]
+
+    reader_comic = Reader(data_url)
+    df_comic_raw = reader_comic.combiner()
+    eda_comics = EDA_per_column(df_comic_raw)
+    while True:
+        print(df_comic_raw.columns)
+        column = input("enter coloumn name")
+        print(df_comic_raw[column].describe())
+        choice = input("enter chart type:1bar, 2bar horizon, 3hist, 4line")
+        if choice == "1":
+            eda_comics.bar_chart()
+        elif choice == "2":
+            eda_comics.barh_chart()
+        elif choice == "3":
+            eda_comics.hist_chart()
+        elif choice == "4":
+            eda_comics.line_chart()
+        elif choice == "0":
+            print("Exiting...")
+
+
+def clean():
+    data_url = [
+        "https://raw.githubusercontent.com/fivethirtyeight/data/master/comic-characters/dc-wikia-data.csv",
+        "https://raw.githubusercontent.com/fivethirtyeight/data/master/comic-characters/marvel-wikia-data.csv",
+    ]
+
+    reader_comic = Reader(data_url)
+    df_comic_raw = reader_comic.combiner()
+    cleaner_comic = Clean(df_comic_raw)
+
+    columns_to_exclude = ["page_id", "urlslug", "EYE", "HAIR", "FIRST APPEARANCE"]
+    df_comic_c = cleaner_comic.delete_columns(columns_to_exclude)
+    print(df_comic_c.columns)
+    cleaner_comic.null_check()
+    df_comic = cleaner_comic.replace_null()
+    cleaner_comic.null_check()
+    return df_comic
+
+
+def Inf():
+    df_comic = clean()
+    analyzer_comic = Analysis(df_comic)
+    analyzer_comic.Align_Sex()
+    analyzer_comic.GSM_Comic()
