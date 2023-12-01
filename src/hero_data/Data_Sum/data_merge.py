b1600b3e1075ba84bae1f5679dde87f451fa9791
jc000222 <1519793475@qq.com>
2023-12-01 17:31:10 -0500

    'add_docstring'

--- src/hero_data/Data_Sum/data_merge.py
+++ src/hero_data/Data_Sum/data_merge.py
@@ -3,16 +3,33 @@ import pandas as pd
 
 class Merger:
     """
-    merge datasets
+    A class to merge datasets based on specified conditions.
+
+    Attributes:
+        df1 (DataFrame): First DataFrame for merging.
+        df2 (DataFrame): Second DataFrame for merging.
+
+    Methods:
+        join(): Merges the specified DataFrames based on the defined conditions.
     """
 
     def __init__(self, df1, df2):
+        """
+        Initialize Merger with provided DataFrames for merging.
+
+        Args:
+        df1 (DataFrame): First DataFrame for merging.
+        df2 (DataFrame): Second DataFrame for merging.
+        """
         self.df1 = df1
         self.df2 = df2
 
     def join(self):
         """
-        join datasets
+        Performs a join operation on the specified DataFrames.
+
+        Returns:
+        DataFrame: The resulting merged DataFrame based on the defined conditions.
         """
         match = pd.read_csv(
             "https://raw.githubusercontent.com/jc000222/Hero_Analysis/main/match.csv"
